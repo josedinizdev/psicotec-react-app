112309d9f9a3dce206af195b6b4cdba0c009daa9
David Douglas Gonçalves Ribeiro <daviddouglas1042@gmail.com>
2022-06-19 01:06:36 -0300

    comment

--- site/src/pages/home/scroll.js
+++ site/src/pages/home/scroll.js
@@ -1,35 +1,35 @@
-const target = document.querySelectorAll('[data-animation]');
-const animacao = 'animate'; 
+// const target = document.querySelectorAll('[data-animation]');
+// const animacao = 'animate'; 
 
-// const debounce = function(func, wait, imediate) {
-//     let timeout;
-//     return function(...args) {
-//         const context = this;
-//         const later = function () {
-//             timeout = null;
-//             if(!imediate) func.aplly(context, args);
-//         };
-//         const callNow = imedi    ate && !timeout;
-//         clearTimeout(timeout);
-//         timeout = setTimeout(later, wait);
-//         if (callNow) func.aplly(context, args);
-//     };
-// };
+// // const debounce = function(func, wait, imediate) {
+// //     let timeout;
+// //     return function(...args) {
+// //         const context = this;
+// //         const later = function () {
+// //             timeout = null;
+// //             if(!imediate) func.aplly(context, args);
+// //         };
+// //         const callNow = imedi    ate && !timeout;
+// //         clearTimeout(timeout);
+// //         timeout = setTimeout(later, wait);
+// //         if (callNow) func.aplly(context, args);
+// //     };
+// // };
 
 
-function animacaoScroll() {
-    const janela = window.pageYOffset + ((window.innerHeight * 3.2) / 4);
-    target.forEach(function(item) {
-        if((janela) > item.offsetTop) {
-            item.classList.add(animacao);
-        }   
-    })
-}   
+// function animacaoScroll() {
+//     const janela = window.pageYOffset + ((window.innerHeight * 3.2) / 4);
+//     target.forEach(function(item) {
+//         if((janela) > item.offsetTop) {
+//             item.classList.add(animacao);
+//         }   
+//     })
+// }   
 
-animacaoScroll();
+// animacaoScroll();
 
-if(target.length) {
-    window.addEventListener('scroll', function() {
-        animacaoScroll();   
-    })
-}
+// if(target.length) {
+//     window.addEventListener('scroll', function() {
+//         animacaoScroll();   
+//     })
+// }
